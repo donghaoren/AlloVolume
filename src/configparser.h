08a291bbbccb64d9fdd173f3c7d973b0e3528ffd
Donghao Ren <donghao.ren@gmail.com>
2014-12-02 15:14:43 -0800

    Update with config parser.

---
+++ src/configparser.h
@@ -0,0 +1,48 @@
+#include <vector>
+#include <yaml-cpp/yaml.h>
+#include <iostream>
+class ConfigParser {
+public:
+    ConfigParser() { }
+    ConfigParser(const std::string& path) {
+        parseFile(path);
+    }
+
+    void parseFile(const std::string& path) {
+        entries.push_back(YAML::LoadFile(path));
+    }
+
+    void parseFile(const std::string& path, const std::string& key) {
+        YAML::Node part = YAML::LoadFile(path)[key];
+        if(!part.IsNull() && part.IsDefined())
+            entries.push_back(part);
+    }
+
+    template<typename T>
+    T get(const std::string& path, const T& fallback = T()) {
+        for(size_t i = 0; i < entries.size(); i++) {
+            YAML::Node node = entries[i];
+            size_t pos = 0;
+            while(pos < path.size()) {
+                std::string subpath;
+                size_t end = path.find('.', pos);
+                if(end == std::string::npos) {
+                    subpath = path.substr(pos);
+                    pos = path.size();
+                } else {
+                    subpath = path.substr(pos, end - pos);
+                    pos = end + 1;
+                }
+                if(!node.IsNull()) {
+                    node.reset(node[subpath]);
+                }
+            }
+            if(!node.IsNull() && node.IsDefined()) {
+                return node.as<T>();
+            }
+        }
+        return fallback;
+    }
+
+    std::vector<YAML::Node> entries;
+};
--- src/renderer.cpp
+++ src/renderer.cpp
@@ -1,6 +1,7 @@
 #include "renderer.h"
 
-#include <yaml-cpp/yaml.h>
+#include "configparser.h"
+
 #include <zmq.h>
 #include <unistd.h>
 
@@ -10,21 +11,20 @@
 
 using namespace std;
 
-YAML::Node config;
+ConfigParser config;
 
 void server() {
     void* zmq_context = zmq_ctx_new();
     void* socket = zmq_socket(zmq_context, ZMQ_PUB);
     int value;
-    value = 200 * 1024 * 1024; zmq_setsockopt(socket, ZMQ_SNDHWM, &value, sizeof(int));
-    value = 64 * 1024 * 1024; zmq_setsockopt(socket, ZMQ_SNDBUF, &value, sizeof(int));
-    value = 1000; zmq_setsockopt(socket, ZMQ_RATE, &value, sizeof(int));
+    value = config.get<int>("zmq.sndhwm", 10000); zmq_setsockopt(socket, ZMQ_SNDHWM, &value, sizeof(int));
+    value = config.get<int>("zmq.rate", 10000000); zmq_setsockopt(socket, ZMQ_RATE, &value, sizeof(int));
 
-    int r = zmq_bind(socket, config["renderer"]["broadcast"].as<string>().c_str());
+    int r = zmq_bind(socket, config.get<string>("renderer.broadcast").c_str());
     if(r < 0) {
         printf("r = %d, %s\n", r, zmq_strerror(errno));
     }
-    int test_size = config["renderer"]["packet_size"].as<int>();
+    int test_size = config.get<int>("renderer.packet_size");
     char* data = new char[test_size];
     for(int i = 0; i < test_size; i++) {
         data[i] = (char)i;
@@ -44,15 +44,14 @@ void client() {
     void* zmq_context = zmq_ctx_new();
     void* socket = zmq_socket(zmq_context, ZMQ_SUB);
     int value;
-    value = 200 * 1024 * 1024; zmq_setsockopt(socket, ZMQ_RCVHWM, &value, sizeof(int));
-    value = 64 * 1024 * 1024; zmq_setsockopt(socket, ZMQ_RCVBUF, &value, sizeof(int));
-    value = 1000; zmq_setsockopt(socket, ZMQ_RATE, &value, sizeof(int));
-    int r = zmq_connect(socket, config["renderer"]["broadcast"].as<string>().c_str());
+    value = config.get<int>("zmq.rcvhwm", 10000); zmq_setsockopt(socket, ZMQ_RCVHWM, &value, sizeof(int));
+    value = config.get<int>("zmq.rate", 10000000); zmq_setsockopt(socket, ZMQ_RATE, &value, sizeof(int));
+    int r = zmq_connect(socket, config.get<string>("renderer.broadcast").c_str());
     if(r < 0) {
         printf("r = %d, %s\n", r, zmq_strerror(errno));
     }
     zmq_setsockopt(socket, ZMQ_SUBSCRIBE, "", 0);
-    int test_size = config["renderer"]["packet_size"].as<int>();
+    int test_size = config.get<int>("renderer.packet_size");
     char* data = new char[test_size];
     while(1) {
         int r = zmq_recv(socket, data, test_size, 0);
@@ -73,7 +72,10 @@ void client() {
 }
 
 int main(int argc, char* argv[]) {
-    config = YAML::LoadFile("allovolume.yaml");
-    if(argc == 2) client();
-    else server();
+    char hostname[256];
+    gethostname(hostname, 256);
+    config.parseFile("allovolume.yaml", hostname);
+    config.parseFile("allovolume.yaml");
+    if(config.get<string>("renderer.role") == "server") server();
+    if(config.get<string>("renderer.role") == "client") client();
 }
