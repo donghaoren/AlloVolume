dc4083c1a8ef83369e80f402b938970e4f9ee3b3
Donghao Ren <donghao.ren@gmail.com>
2014-12-10 15:02:50 -0800

    Added hd rendering script

---
+++ controller/hd_rendering.py
@@ -0,0 +1,3 @@
+from server import controller_client
+
+controller_client.HDRendering("aa.png", 3000, 2000)
--- controller/server/controller_client.py
+++ controller/server/controller_client.py
@@ -2,6 +2,7 @@ import allovolume_protocol_pb2 as protocol
 
 import zmq
 import struct
+import math
 
 zmq_context = zmq.Context()
 
@@ -9,7 +10,27 @@ controller = zmq.Socket(zmq_context, zmq.REQ)
 controller.connect("tcp://127.0.0.1:55556")
 
 
-def HDRendering(filename, lens, width = 3000, height = 2000):
+def HDRendering(filename, width = 3000, height = 2000, lens = "perspective", fovx = 90):
     msg = protocol.ControllerRequest()
+
     msg.type = protocol.ControllerRequest.HDRendering
-    msg.hd_rendering_task
+
+    if lens == "perspective":
+        msg.hd_rendering_task.lens_type = protocol.HDRenderingTask.Perspective
+        msg.hd_rendering_task.perspective_fovx = fovx / 180.0 * math.pi
+    if lens == "equirectangular":
+        msg.hd_rendering_task.lens_type = protocol.HDRenderingTask.Equirectangular
+
+    msg.hd_rendering_task.total_width = width
+    msg.hd_rendering_task.total_height = height
+
+    msg.hd_rendering_task.output_filename = filename
+
+    msg.hd_rendering_task.lens_parameters.eye_separation = 0
+    msg.hd_rendering_task.lens_parameters.focal_distance = 1
+
+    controller.send(msg.SerializeToString())
+
+    result = protocol.ControllerResponse()
+    result.ParseFromString(controller.recv())
+    print result
