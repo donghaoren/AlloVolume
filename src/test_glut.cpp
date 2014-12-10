f1ce0b32ff91f4ee81043a988e3e5794aa320915
Donghao Ren <donghao.ren@gmail.com>
2014-12-09 21:28:36 -0800

    Updated.

--- src/test_glut.cpp
+++ src/test_glut.cpp
@@ -15,38 +15,31 @@ void display_callback() {
 }
 
 void keyboard_callback(unsigned char key, int x, int y) {
-    if(key == 'a') {
-        glutEnterGameMode();
-        glutDisplayFunc(display_callback);
-        glutKeyboardFunc(keyboard_callback);
-    } else if(key == 'b') {
-        glutLeaveGameMode();
-        glutDisplayFunc(display_callback);
-        glutKeyboardFunc(keyboard_callback);
-    } else {
-        exit(0);
-    }
+    exit(0);
 }
 
 int main(int argc, char* argv[]) {
     glutInit(&argc, argv);
 
-    int width = glutGet(GLUT_SCREEN_WIDTH);
-    int height = glutGet(GLUT_SCREEN_HEIGHT);
+    glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE | GLUT_STEREO);
 
-    printf("%d %d\n", width, height);
+    bool game_mode = (argc == 1);
 
-    char game_mode_string[64];
-    sprintf(game_mode_string, "%dx%d:24", width, height);
-
-    glutGameModeString(game_mode_string);
-
-    glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE);
-
-    glutCreateWindow("AlloVolume");
-
-    glutDisplayFunc(display_callback);
-    glutKeyboardFunc(keyboard_callback);
+    if(game_mode) {
+        int width = glutGet(GLUT_SCREEN_WIDTH);
+        int height = glutGet(GLUT_SCREEN_HEIGHT);
+        printf("%d %d\n", width, height);
+        char game_mode_string[64];
+        sprintf(game_mode_string, "%dx%d:24", width, height);
+        glutGameModeString(game_mode_string);
+        glutEnterGameMode();
+        glutDisplayFunc(display_callback);
+        glutKeyboardFunc(keyboard_callback);
+    } else {
+        glutCreateWindow("AlloVolume");
+        glutDisplayFunc(display_callback);
+        glutKeyboardFunc(keyboard_callback);
+    }
 
     glutMainLoop();
 }
